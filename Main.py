081ed93f78e881beec497a2910398467067013ea
Jhon Barragan <jhonbarragan99@gmail.com>
2025-01-28 19:28:29 -0500

    Añadir un nuevo archivo

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
+## git add main.py para agregar archivos a git
+##git commit -m "Añadi un archivo de python" sirve para ingresar un comentario en el git
+##git push origin main sirve para subir el archivo a la pagina Github
+## git branch sirve para saber en que rama estoy del archivo 
+##git checkout dev (nombre del archivo) sirve para salir de una rama 
+
 def suma(a,b):
     return a+b 
 
